95407d375f3fceaf4b663e4075b542f9b9924031
David <davjue@gmail.com>
2017-08-22 20:57:07 -0700

    finished

--- js/app.js
+++ js/app.js
@@ -5,27 +5,33 @@ $(document).ready(function() {
     let selections = {"zero":"a", "one":"b", "two":"c", "three":"d",
         "four":"e", "five":"f", "six":"g", "seven":"h", "eight":"i"}
     let counter = 0;
+    let xScore = 0;
+    let oScore = 0;
     let win = false;
-
-    if (win === false) {
         $("#zero, #one,#two,#three,#four,#five,#six,#seven,#eight").on("click", function () {
-            if (this.innerHTML.length != 1) {
-                counter += 1;
-                if (counter % 2 === 0) {
-                    this.innerHTML = "X";
-                    selections[this.id] = "X";
-                    console.log(selections);
+            if (win===false) {
+                if (this.innerHTML.length != 1) {
+                    counter += 1;
+                    if (counter % 2 === 0) {
+                        this.innerHTML = "X";
+                        selections[this.id] = "X";
+                        $(".user").text("O's turn");
 
-                }
-                else {
-                    this.innerHTML = "O";
-                    selections[this.id] = "O";
-                    console.log(selections);
+                    }
+                    else {
+                        this.innerHTML = "O";
+                        selections[this.id] = "O";
+                        $(".user").text("X's turn");
+                    }
                 }
             }
             checkWinner();
+            if (win === true) {
+                selections = {"zero":"a", "one":"b", "two":"c", "three":"d",
+                    "four":"e", "five":"f", "six":"g", "seven":"h", "eight":"i"}
+            }
         });
-    };
+    console.log(win+"zzz");
     function checkWinner() {
         if (counter >= 5) {
             if ((selections["zero"] === selections["one"] && selections["zero"] === selections["two"]) ||
@@ -34,15 +40,22 @@ $(document).ready(function() {
                 (selections["two"] === selections["five"] && selections["two"] === selections["eight"]) ||
                 (selections["two"] === selections["four"] && selections["two"] === selections["six"]) ||
                 (selections["three"] === selections["four"] && selections["three"] === selections["five"]) ||
+                (selections["one"] === selections["four"] && selections["one"] === selections["seven"]) ||
                 (selections["six"] === selections["seven"] && selections["six"] === selections["eight"])
             ) {
                 if (counter % 2 === 0) {
-                    alert("X is the winner!");
+                    alert("X is the winner!  Click reset to play again.");
+                    $('.user').html("X is the winner!");
                     win = true;
+                    xScore += 1;
+                    $(".x-score").html(`X score: ${xScore}`);
                 }
                 else {
-                    alert("O is the winner!");
+                    alert("O is the winner!  Click reset to play again.");
+                    $('.user').html("O is the winner!");
                     win = true;
+                    oScore += 1;
+                    $(".o-score").html(`O score: ${oScore}`);
                 }
             }
             if (counter === 9) {
@@ -59,6 +72,8 @@ $(document).ready(function() {
         for (i = 0; i<9; i++) {
             $(".box")[i].innerHTML = "";
         }
+        $(".user").text("O's turn");
+        win = false;
     })
 
 });
